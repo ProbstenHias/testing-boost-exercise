8a48aac319569d0ed27f8526e2883cb113cebb16
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-02-02 18:55:11 +0100

    Fix formatting

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -9,14 +9,13 @@ BOOST_AUTO_TEST_CASE(OpenData)
 {
   const std::string fileName{"../data/m3.csv"};
   const int         expectedMatrixSize{3};
-  MatrixXd expectedMatrix{3, 3};
+  MatrixXd          expectedMatrix{3, 3};
   expectedMatrix << 0.680375, 0.59688, -0.329554,
-                    -0.211234, 0.823295, 0.536459,
-                    0.566198, -0.604897, -0.444451;
+      -0.211234, 0.823295, 0.536459,
+      0.566198, -0.604897, -0.444451;
 
   const Eigen::MatrixXd actualMatrix{matrixIO::openData(fileName, expectedMatrixSize)};
   BOOST_TEST(expectedMatrix == actualMatrix);
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
